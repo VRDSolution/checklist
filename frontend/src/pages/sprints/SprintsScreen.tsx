0b837d8da6828e359b713ea38d1cb40982e169f2
Guilherme Zambrano Bohn <guilherrme.bohn@gmail.com>
2026-01-13 17:59:17 -0300

    Style: again btn

--- frontend/src/pages/sprints/SprintsScreen.tsx
+++ frontend/src/pages/sprints/SprintsScreen.tsx
@@ -236,9 +236,9 @@ export const SprintsScreen: React.FC<SprintsScreenProps> = ({ onNavigate }) => {
             
             <Button 
               onClick={() => setIsCreateModalOpen(true)}
-              className="p-2 md:w-[200px] md:px-4 md:py-2 flex items-center justify-center flex-none"
+              className="!w-[40px] !h-[40px] !p-0 md:!w-[200px] md:!h-auto md:!px-4 md:!py-2 flex items-center justify-center flex-none"
             >
-              <Plus className="w-4 h-4 mr-0 md:mr-2" />
+              <Plus className="w-5 h-5 mr-0 md:mr-2" />
               <span className="hidden md:inline">Novo Sprint</span>
             </Button>
           </div>
