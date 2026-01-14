81ab4ad8abbdc9c56b9c89f4bda37735ead937da
Guilherme Zambrano Bohn <guilherrme.bohn@gmail.com>
2026-01-14 14:16:17 -0300

    feat: navegação dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import MobileApp from '@/pages/MobileApp'
-import { OfflineIndicator } from '@/components/common/OfflineIndicator'
+import React from 'react'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './contexts/AuthContext'
+import { DataProvider } from './contexts/DataContext'
+import { AppRoutes } from './routes'
+import { OfflineIndicator } from './components/common/OfflineIndicator'
 
 function App() {
   return (
-    <>
-      <Toaster position="top-right" />
-      <OfflineIndicator />
-      <MobileApp />
-    </>
+    <AuthProvider>
+      <DataProvider>
+        <div className="min-h-screen bg-slate-50 font-sans text-slate-900">
+          <Toaster position="top-right" />
+          <OfflineIndicator />
+          <AppRoutes />
+        </div>
+      </DataProvider>
+    </AuthProvider>
   )
 }
 
